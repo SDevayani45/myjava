02a2ec3bdb2292d73fff2484a0d75d9aefa831fd
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-06 17:01:06 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	char c='a';
+	if((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	System.out.print(c + "alphabet");
+	else
+	System.out.print(c + "not an alphabet");
+}
+}
