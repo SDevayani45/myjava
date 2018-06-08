b7ed2c5282842a1bf774167f976d446d4475ac99
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-08 12:05:16 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,14 @@
+public class power
+{
+   public static void main(String args[])
+   {
+int a=2,b=3,ans=1;
+int i=b;
+while(i>0)
+     {
+ans=ans*a;
+i--;
+    }
+System.out.print("the power of  "+a+" =",+ans);
+   }
+}
