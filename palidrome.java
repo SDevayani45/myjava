22f601057019a428182081fbc0056afb6477b689
SDevayani45 <36623877+SDevayani45@users.noreply.github.com>
2018-06-08 10:59:00 +0530

    Create palidrome.java

---
+++ palidrome.java
@@ -0,0 +1,23 @@
+public class palindrome{
+
+     public static void main(String []args){
+       int r,sum=0,temp;
+       int n=454;
+       temp=n;
+       while(n>0)
+       {
+           r=n%10;
+           sum=(sum*10)+r;
+           n=n/10;
+           
+       }
+       if(temp==sum)
+       {
+           System.out.print("palindrome");
+       }
+       else
+       {
+           System.out.println("not");
+       }
+     }
+}
